de6847d84f6c92707bd63ad845c556840a994aa7
hlm12348765 <15620956546hlm@gmail.com>
2017-04-06 13:54:49 +0800

    first commit

---
+++ xunlian.cpp
@@ -0,0 +1,14 @@
+/* xunlian.cpp */
+#include <QtGui>
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+	QApplication app(argc, argv);
+	QWidget *windown = new Qwidget;
+	window->setWindowTitle("模拟训练");
+	QTextCodec::setCodecForCStrings(QTextCodec::codeForName("UTF-8"));
+	
+	window->show();
+	return app.exec();
+}	
